edbec77547a67203956c836aa0a319bb1b414b00
amamenko <amamenko@hotmail.com>
2021-03-15 20:54:45 -0400

    Added server-side instagram challenge fix for heroku

--- index.js
+++ index.js
@@ -5,32 +5,42 @@ const fs = require("fs");
 const Jimp = require("jimp");
 const Instagram = require("instagram-web-api");
 const cron = require("node-cron");
+const imaps = require("imap-simple");
+const _ = require("lodash");
+const simpleParser = require("mailparser").simpleParser;
 
 require("dotenv").config();
 
 const port = process.env.PORT || 4000;
 
-// Upload new Inky Doodle to Instagram every day at 4:00 PM
-cron.schedule("0 16 * * *", () => {
-  const client = new Instagram({
-    username: process.env.INSTAGRAM_USERNAME,
-    password: process.env.INSTAGRAM_PASSWORD,
-  });
+// Upload new Inky Doodle to Instagram every day at 9:00 PM
+cron.schedule("0 21 * * *", async () => {
+  const client = new Instagram(
+    {
+      username: process.env.INSTAGRAM_USERNAME,
+      password: process.env.INSTAGRAM_PASSWORD,
+    },
+    {
+      language: "en-US",
+      proxy: process.NODE_ENV === "production" ? process.FIXIE_URL : undefined,
+    }
+  );
 
-  client.login().then(() => {
-    client
-      .getPhotosByUsername({ username: process.env.INSTAGRAM_USERNAME })
-      .then(
-        (res) =>
-          res.user.edge_owner_to_timeline_media.edges.map(
-            (item) => item.node.edge_media_to_caption.edges[0].node.text
-          )[0]
-      )
-      .then((mostRecent) => Number(mostRecent.split(" - ")[0]))
-      .then((latestNumber) => {
-        const updatedNumber = latestNumber + 1;
+  try {
+    client.login().then(() => {
+      client
+        .getPhotosByUsername({ username: process.env.INSTAGRAM_USERNAME })
+        .then(
+          (res) =>
+            res.user.edge_owner_to_timeline_media.edges.map(
+              (item) => item.node.edge_media_to_caption.edges[0].node.text
+            )[0]
+        )
+        .then((mostRecent) => Number(mostRecent.split(" - ")[0]))
+        .then((latestNumber) => {
+          const updatedNumber = latestNumber + 1;
 
-        const inkyDoodleQuery = `
+          const inkyDoodleQuery = `
         query {
             inkyDoodleCollection(where: {number: ${updatedNumber}}) {
                 items   {
@@ -46,67 +56,136 @@ cron.schedule("0 16 * * *", () => {
     }
 `;
 
-        axios({
-          url: `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
-          method: "post",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
-          },
-          data: {
-            query: inkyDoodleQuery,
-          },
-        })
-          .then((res) => res.data)
-          .then(async ({ data, errors }) => {
-            if (errors) {
-              console.error(errors);
-            }
+          axios({
+            url: `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+            method: "post",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+            },
+            data: {
+              query: inkyDoodleQuery,
+            },
+          })
+            .then((res) => res.data)
+            .then(async ({ data, errors }) => {
+              if (errors) {
+                console.error(errors);
+              }
 
-            const updatedInkyDoodle = data.inkyDoodleCollection.items[0];
+              const updatedInkyDoodle = data.inkyDoodleCollection.items[0];
 
-            if (updatedInkyDoodle) {
-              const updatedCaption = `${updatedNumber} - ${
-                updatedInkyDoodle.name
-              }\n${
-                updatedInkyDoodle.parents
-                  ? updatedInkyDoodle.parents.length > 0
-                    ? updatedInkyDoodle.parents
-                        .map((parent) => "#" + parent)
-                        .join(" + ") + " \n"
+              if (updatedInkyDoodle) {
+                const updatedCaption = `${updatedNumber} - ${
+                  updatedInkyDoodle.name
+                }\n${
+                  updatedInkyDoodle.parents
+                    ? updatedInkyDoodle.parents.length > 0
+                      ? updatedInkyDoodle.parents
+                          .map((parent) => "#" + parent)
+                          .join(" + ") + " \n"
+                      : ""
                     : ""
-                  : ""
-              }#inkydoodle #gen${updatedInkyDoodle.generation}`;
+                }#inkydoodle #gen${updatedInkyDoodle.generation}`;
 
-              Jimp.read(updatedInkyDoodle.image.url)
-                .then((lenna) => {
-                  return lenna
-                    .resize(405, 405, Jimp.RESIZE_NEAREST_NEIGHBOR)
-                    .quality(100)
-                    .write(`./${updatedInkyDoodle.name}.jpg`, async () => {
-                      // Upload converted and resized JPG to Instagram feed
-                      await client
-                        .uploadPhoto({
-                          photo: `${updatedInkyDoodle.name}.jpg`,
-                          caption: updatedCaption,
-                          post: "feed",
-                        })
-                        .then(({ media }) => {
-                          console.log(
-                            `https://www.instagram.com/p/${media.code}/`
-                          );
-                          // Remove Local JPG File
-                          fs.unlinkSync(`${updatedInkyDoodle.name}.jpg`);
-                        });
-                    });
-                })
-                .catch((err) => {
-                  console.log(err);
+                Jimp.read(updatedInkyDoodle.image.url)
+                  .then((lenna) => {
+                    return lenna
+                      .resize(405, 405, Jimp.RESIZE_NEAREST_NEIGHBOR)
+                      .quality(100)
+                      .write(`./${updatedInkyDoodle.name}.jpg`, async () => {
+                        // Upload converted and resized JPG to Instagram feed
+                        await client
+                          .uploadPhoto({
+                            photo: `${updatedInkyDoodle.name}.jpg`,
+                            caption: updatedCaption,
+                            post: "feed",
+                          })
+                          .then(({ media }) => {
+                            console.log(
+                              `https://www.instagram.com/p/${media.code}/`
+                            );
+                            // Remove Local JPG File
+                            fs.unlinkSync(`${updatedInkyDoodle.name}.jpg`);
+                          });
+                      });
+                  })
+                  .catch((err) => {
+                    console.log(err);
+                  });
+              }
+            });
+        });
+    });
+  } catch (err) {
+    // Instagram has thrown a checkpoint error
+    if (err.error && err.error.message === "checkpoint_required") {
+      const challengeUrl = err.error.checkpoint_url;
+
+      await client.updateChallenge({ challengeUrl, choice: 1 });
+
+      const emailConfig = {
+        imap: {
+          user: `${process.env.INKY_DOODLE_EMAIL}`,
+          password: `${process.env.INKY_DOODLE_EMAIL_PASSWORD}`,
+          host: "imap.gmail.com",
+          port: 993,
+          tls: true,
+          tlsOptions: {
+            servername: "imap.gmail.com",
+            rejectUnauthorized: false,
+          },
+          authTimeout: 30000,
+        },
+      };
+
+      // Connect to email and solve Instagram challenge
+      imaps.connect(emailConfig).then(async (connection) => {
+        return connection.openBox("INBOX").then(async () => {
+          const searchCriteria = ["UNSEEN"];
+          const fetchOptions = {
+            bodies: [""],
+          };
+          return connection
+            .search(searchCriteria, fetchOptions)
+            .then((messages) => {
+              messages.forEach((item) => {
+                const all = _.find(item.parts, { which: "" });
+                const id = item.attributes.uid;
+                const idHeader = "Imap-Id: " + id + "\r\n";
+                simpleParser(idHeader + all.body, async (err, mail) => {
+                  if (err) {
+                    console.log(err);
+                  }
+
+                  const answerCodeArr = mail.text
+                    .split("\n")
+                    .filter(
+                      (item) =>
+                        item && /^\S+$/.test(item) && !isNaN(Number(item))
+                    );
+
+                  if (mail.text.includes("Instagram")) {
+                    if (answerCodeArr.length > 0) {
+                      const answerCode = Number(answerCodeArr[0]);
+
+                      await client.updateChallenge({
+                        challengeUrl,
+                        securityCode: answerCode,
+                      });
+
+                      console.log(
+                        `Answered Instagram security challenge with answer code: ${answerCode}`
+                      );
+                    }
+                  }
                 });
-            }
-          });
+              });
+            });
+        });
       });
-  });
+    }
+  }
 });
 
 app.listen(port, () => {
