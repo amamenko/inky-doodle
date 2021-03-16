d66aafd5f2b2580f0868c96e5ec95cc2457abac9
Avi Mamenko <49536715+amamenko@users.noreply.github.com>
2021-03-16 11:48:48 -0400

    Changed Instagram post logic to async function

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ require("dotenv").config();
 
 const port = process.env.PORT || 4000;
 
-// Upload new Inky Doodle to Instagram every day at 11:18 AM
-cron.schedule("18 11 * * *", async () => {
+// Upload new Inky Doodle to Instagram every day at 11:52 AM
+cron.schedule("52 11 * * *", async () => {
     const instagramLoginFunction = async () => {
         const client = new Instagram(
             {
@@ -27,7 +27,96 @@ cron.schedule("18 11 * * *", async () => {
             }
         );
 
-        console.log(process.env.FIXIE_URL);
+        const instagramPostPictureFunction = async () => {
+            await client
+                .getPhotosByUsername({ username: process.env.INSTAGRAM_USERNAME })
+                .then(
+                    (res) =>
+                        res.user.edge_owner_to_timeline_media.edges.map(
+                            (item) => item.node.edge_media_to_caption.edges[0].node.text
+                        )[0]
+                )
+                .then((mostRecent) => Number(mostRecent.split(" - ")[0]))
+                .then((latestNumber) => {
+                    const updatedNumber = latestNumber + 1;
+
+                    const inkyDoodleQuery = `
+        query {
+            inkyDoodleCollection(where: {number: ${updatedNumber}}) {
+                items   {
+                number
+                generation
+                name
+                parents
+                image {
+                    url
+                }
+            }
+        }
+    }
+`;
+
+                    axios({
+                        url: `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+                        method: "post",
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+                        },
+                        data: {
+                            query: inkyDoodleQuery,
+                        },
+                    })
+                        .then((res) => res.data)
+                        .then(async ({ data, errors }) => {
+                            if (errors) {
+                                console.error(errors);
+                            }
+
+                            const updatedInkyDoodle = data.inkyDoodleCollection.items[0];
+
+                            if (updatedInkyDoodle) {
+                                const updatedCaption = `${updatedNumber} - ${
+                                    updatedInkyDoodle.name
+                                    }\n${
+                                    updatedInkyDoodle.parents
+                                        ? updatedInkyDoodle.parents.length > 0
+                                            ? updatedInkyDoodle.parents
+                                                .map((parent) => "#" + parent)
+                                                .join(" + ") + " \n"
+                                            : ""
+                                        : ""
+                                    }#inkydoodle #gen${updatedInkyDoodle.generation}`;
+
+                                Jimp.read(updatedInkyDoodle.image.url)
+                                    .then((lenna) => {
+                                        return lenna
+                                            .resize(405, 405, Jimp.RESIZE_NEAREST_NEIGHBOR)
+                                            .quality(100)
+                                            .write(`./${updatedInkyDoodle.name}.jpg`, async () => {
+                                                // Upload converted and resized JPG to Instagram feed
+                                                await client
+                                                    .uploadPhoto({
+                                                        photo: `${updatedInkyDoodle.name}.jpg`,
+                                                        caption: updatedCaption,
+                                                        post: "feed",
+                                                    })
+                                                    .then(({ media }) => {
+                                                        console.log(
+                                                            `https://www.instagram.com/p/${media.code}/`
+                                                        );
+                                                        // Remove Local JPG File
+                                                        fs.unlinkSync(`${updatedInkyDoodle.name}.jpg`);
+                                                    });
+                                            });
+                                    })
+                                    .catch((err) => {
+                                        console.log(err);
+                                    });
+                            }
+                        });
+                });
+        }
 
         try {
             console.log("Logging in...");
@@ -35,6 +124,8 @@ cron.schedule("18 11 * * *", async () => {
             await client.login();
 
             console.log("Login successful!");
+
+            await instagramPostPictureFunction();
         } catch (err) {
             console.log("Login failed!");
 
@@ -112,6 +203,8 @@ cron.schedule("18 11 * * *", async () => {
                                                         console.log(
                                                             `Answered Instagram security challenge with answer code: ${answerCode}`
                                                         );
+
+                                                        await instagramPostPictureFunction();
                                                     }
                                                 }
                                             });
@@ -122,112 +215,12 @@ cron.schedule("18 11 * * *", async () => {
                     }, timeout);
                 }
 
-                await delayedEmailFunction(45000);
+                await delayedEmailFunction(35000);
             }
         }
     }
 
-    const client = await instagramLoginFunction();
-
-    if (!client.authenticated) {
-        await client.login({
-            username: process.env.INSTAGRAM_USERNAME,
-            password: process.env.INSTAGRAM_PASSWORD,
-        },
-            {
-                language: "en-US",
-                proxy: process.NODE_ENV === "production" ? process.env.FIXIE_URL : undefined,
-            });
-    }
-
-    await client
-        .getPhotosByUsername({ username: process.env.INSTAGRAM_USERNAME })
-        .then(
-            (res) =>
-                res.user.edge_owner_to_timeline_media.edges.map(
-                    (item) => item.node.edge_media_to_caption.edges[0].node.text
-                )[0]
-        )
-        .then((mostRecent) => Number(mostRecent.split(" - ")[0]))
-        .then((latestNumber) => {
-            const updatedNumber = latestNumber + 1;
-
-            const inkyDoodleQuery = `
-        query {
-            inkyDoodleCollection(where: {number: ${updatedNumber}}) {
-                items   {
-                number
-                generation
-                name
-                parents
-                image {
-                    url
-                }
-            }
-        }
-    }
-`;
-
-            axios({
-                url: `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
-                method: "post",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
-                },
-                data: {
-                    query: inkyDoodleQuery,
-                },
-            })
-                .then((res) => res.data)
-                .then(async ({ data, errors }) => {
-                    if (errors) {
-                        console.error(errors);
-                    }
-
-                    const updatedInkyDoodle = data.inkyDoodleCollection.items[0];
-
-                    if (updatedInkyDoodle) {
-                        const updatedCaption = `${updatedNumber} - ${
-                            updatedInkyDoodle.name
-                            }\n${
-                            updatedInkyDoodle.parents
-                                ? updatedInkyDoodle.parents.length > 0
-                                    ? updatedInkyDoodle.parents
-                                        .map((parent) => "#" + parent)
-                                        .join(" + ") + " \n"
-                                    : ""
-                                : ""
-                            }#inkydoodle #gen${updatedInkyDoodle.generation}`;
-
-                        Jimp.read(updatedInkyDoodle.image.url)
-                            .then((lenna) => {
-                                return lenna
-                                    .resize(405, 405, Jimp.RESIZE_NEAREST_NEIGHBOR)
-                                    .quality(100)
-                                    .write(`./${updatedInkyDoodle.name}.jpg`, async () => {
-                                        // Upload converted and resized JPG to Instagram feed
-                                        await client
-                                            .uploadPhoto({
-                                                photo: `${updatedInkyDoodle.name}.jpg`,
-                                                caption: updatedCaption,
-                                                post: "feed",
-                                            })
-                                            .then(({ media }) => {
-                                                console.log(
-                                                    `https://www.instagram.com/p/${media.code}/`
-                                                );
-                                                // Remove Local JPG File
-                                                fs.unlinkSync(`${updatedInkyDoodle.name}.jpg`);
-                                            });
-                                    });
-                            })
-                            .catch((err) => {
-                                console.log(err);
-                            });
-                    }
-                });
-        });
+    await instagramLoginFunction();
 });
 
 app.listen(port, () => {
